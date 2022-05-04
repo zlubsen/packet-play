7fac32de53cca89be15c8e93cdbdbdd992df8ea6
Zeeger <zeeger@lubsen.eu>
2022-05-04 21:47:44 +0200

    Work...

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod model;
 
+use std::env;
 use std::fs::File;
-use std::net::{Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::process::exit;
+use std::time::{Duration, Instant};
 use clap::Parser;
+use log::{info, error, trace};
 use crate::model::Recording::PCAP;
+
+const DEFAULT_DEST_PORT : u16 = 3000;
+const DEFAULT_SRC_PORT : u16 = 3000;
+const DEFAULT_TTL : u32 = 1;
 
 #[derive(Parser, Debug)]
 #[clap(name = "packet-play")]
@@ -13,24 +20,90 @@ struct Cli {
     file: String,
     #[clap(parse(try_from_str))]
     destination: Option<SocketAddr>,
-    ttl: Option<u8>,
+    #[clap(short = 's', long = "source")]
+    source_port: Option<u16>,
+    ttl: Option<u32>,
 }
 
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+
     let cli = Cli::parse();
     let filename : String = cli.file;
     let destination = cli.destination.unwrap_or(SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(),3000));
+    let ttl : u32 = cli.ttl.unwrap_or(DEFAULT_TTL);
 
-    println!("file: {filename}");
+    info!("Loading file: {filename}");
+    info!("Replaying to destination: {destination}");
+
     let file_path = std::path::Path::new(filename.as_str());
-    let recording = if file_path.is_file() && file_path.exists() {
-        File::open(file_path)
-    } else {
-        println!("Provided file {filename} is not a file or does not exist.");
+    if !file_path.is_file() || !file_path.exists() {
+        error!("Provided file {filename} is not a file or does not exist.");
         exit(1);
     };
-    println!("recording: {recording:?}");
-    println!("dest {destination}");
 
+    let player = Player::new(filename).destination(destination).ttl(ttl);
+    player.play();
+}
 
+struct Player {
+    file_path: String,
+    destination: SocketAddr,
+    source_port: u16,
+    ttl: u32,
+}
+
+impl Player {
+    pub fn new(file_path: String) -> Self {
+        Self {
+            file_path,
+            destination: SocketAddr::new(IpAddr::V4(Ipv4Addr::BROADCAST),DEFAULT_DEST_PORT),
+            source_port: DEFAULT_SRC_PORT,
+            ttl: DEFAULT_TTL,
+        }
+    }
+
+    pub fn file(self, file_path: String) -> Self {
+        Self {
+            file_path,
+            ..self
+        }
+    }
+
+    pub fn destination(self, destination: SocketAddr) -> Self {
+        Self {
+            destination,
+            ..self
+        }
+    }
+
+    pub fn ttl(self, ttl: u32) -> Self {
+        Self {
+            ttl,
+            ..self
+        }
+    }
+
+    pub fn play(&self) {
+        let socket = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], self.source_port)),).expect(format!("Failed to bind socket {:?}", self.destination).as_str());
+        socket.set_broadcast(true);
+        socket.set_ttl(self.ttl);
+        let mut file = File::open(self.file_path.clone()).unwrap();
+        let recording = model::pcap::Pcap::try_from(file);
+        if let Ok(recording) = recording {
+            info!("ready to play: {:?}", recording.header);
+            let start_time = recording.packets.first().unwrap().ts_secs;
+            for packet in recording.packets {
+                trace!("packet - secs: {} - len: {}", packet.ts_secs, packet.captured_packet_length);
+                socket.send_to(&packet.packet_data.as_slice()[..8], self.destination).expect("Could not send packet");
+                std::thread::sleep(Duration::from_millis(500));
+            }
+        } else {
+            let error = recording.unwrap_err();
+            error!("Cannot play recording, because: {:?}", error);
+        }
+    }
 }
--- src/model/pcap.rs
+++ src/model/pcap.rs
@@ -1,14 +1,19 @@
 use std::fs::File;
-use std::io::Read;
-use bytes::Bytes;
+use std::io::{BufReader, Read};
+use log::trace;
+use nom::bytes::complete::take;
 use nom::IResult;
+use nom::multi::many1;
+use nom::number::complete::{be_u16, be_u32};
 use crate::model::Errors;
 
+#[derive(Debug)]
 pub struct Pcap {
     pub header: PcapFileHeader,
     pub packets: Vec<PcapPacketRecord>,
 }
 
+#[derive(Debug)]
 pub struct PcapFileHeader {
     pub magic_number: PcapMagicNumber,
     pub major_version: u16,
@@ -19,11 +24,22 @@ pub struct PcapFileHeader {
     pub f_bit: bool,
 }
 
+#[derive(Debug)]
 pub enum PcapMagicNumber {
-    MS(u32), // 0xA1B2C3D4
-    NS(u32), // 0xA1B23C4D
+    MILLIS, // 0xA1B2C3D4
+    NANOS, // 0xA1B23C4D
 }
 
+impl From<u32> for PcapMagicNumber {
+    fn from(value: u32) -> Self {
+        match value {
+            0xA1B23C4D => PcapMagicNumber::NANOS,
+            0xA1B2C3D4 | _ => PcapMagicNumber::MILLIS,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct PcapPacketRecord {
     pub ts_secs: u32,
     pub ts_secs_fraction: u32,
@@ -36,19 +52,28 @@ impl TryFrom<File> for Pcap {
     type Error = Errors;
 
     fn try_from(mut file: File) -> Result<Self, Self::Error> {
-        let mut buf = Bytes::new();
-        let bytes_read = file.read(&mut buf).unwrap();
-        if let Ok((input, pcap)) = parse_pcap(&buf) {
-            Ok(pcap)
-        } else {
-            Err(Errors::ParsePcapError)
+        let mut buf = Vec::<u8>::new();
+        let mut reader = BufReader::new(file);
+        let _read = reader.read_to_end(&mut buf);
+        trace!("start parsing pcap file");
+        match parse_pcap_file(&buf) {
+            Ok((input, pcap)) => { Ok(pcap) }
+            Err(err) => { trace!("{err}"); Err(Errors::ParsePcapError) }
         }
+        // if let Ok((input, pcap)) = parse_pcap_file(&buf) {
+        //     Ok(pcap)
+        // } else {
+        //     Err(Errors::ParsePcapError)
+        // }
     }
 }
 
-fn parse_pcap(input: &[u8]) -> IResult<&[u8], Pcap> {
+fn parse_pcap_file(input: &[u8]) -> IResult<&[u8], Pcap> {
+    trace!("parse the file");
     let (input, header) = pcap_header(input)?;
-    let (input, packets) = pcap_packets(input)?;
+    trace!("parsed the header");
+    let (input, packets) = many1(pcap_packet_record)(input)?;
+    trace!("parsed the packets");
     Ok((input, Pcap {
         header,
         packets,
@@ -56,9 +81,45 @@ fn parse_pcap(input: &[u8]) -> IResult<&[u8], Pcap> {
 }
 
 fn pcap_header(input: &[u8]) -> IResult<&[u8], PcapFileHeader> {
-    todo!()
+    trace!("parse the header");
+    let (input, magic_number) = be_u32(input)?;
+    let (input, major_version) = be_u16(input)?;
+    let (input, minor_version) = be_u16(input)?;
+    let (input, _reserved) = take(8usize)(input)?;
+    let (input, snap_len) = be_u32(input)?;
+    let (input, link_type) = be_u32(input)?;
+    let frame_cyclic_sequence : u8 = ((link_type & 0xF0000000) >> 28) as u8;
+    let f_bit = if (link_type & 0x10000000) >> 27 == 0 {false} else {true};
+
+    let magic_number = PcapMagicNumber::from(magic_number);
+
+    Ok((input, PcapFileHeader {
+        magic_number,
+        major_version,
+        minor_version,
+        snap_len,
+        link_type,
+        frame_cyclic_sequence,
+        f_bit,
+    }))
 }
 
-fn pcap_packets(input: &[u8]) -> IResult<&[u8], Vec<PcapPacketRecord>> {
-    todo!()
+fn pcap_packet_record(input: &[u8]) -> IResult<&[u8], PcapPacketRecord> {
+    trace!("parse a packet record");
+    let (input, ts_secs) = be_u32(input)?;
+    let (input, ts_secs_fraction) = be_u32(input)?;
+    let (input, captured_packet_length) = be_u32(input)?;
+    let (input, original_packet_length) = be_u32(input)?;
+    trace!("parsed packet record fields");
+    let (input, packet_data) = take(captured_packet_length)(input)?;
+    trace!("parsed packet record data");
+
+    let packet_data = packet_data.to_vec();
+    Ok((input, PcapPacketRecord {
+        ts_secs,
+        ts_secs_fraction,
+        captured_packet_length,
+        original_packet_length,
+        packet_data,
+    }))
 }
